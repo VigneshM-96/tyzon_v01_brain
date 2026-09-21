ee7bcdf252edad2a931fbadd1f8f0ee981399524
VigneshM-96 <vignesh251974@gmail.com>
2026-09-21 10:13:13 +0530

    added the api call of llm

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-print("tyzon testing")
+import os
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENROUTER_API_KEY")
+url = "https://openrouter.ai/api/v1/chat/completions"
+
+headers={
+    "Authorization":f"Bearer {API_KEY}",
+    "Content-Type":"application/json"
+}
+
+data={
+    "model":"openai/gpt-chat-latest",
+    "messages":[
+        {
+            "role":"user",
+            "content":"Tell me about ASI in AI era"
+        }
+    ]
+}
+
+response = requests.post(
+    url,
+    headers=headers,
+    json=data
+)
+
+result = response.json()
+
+print(result["choices"][0]["message"]["content"])
